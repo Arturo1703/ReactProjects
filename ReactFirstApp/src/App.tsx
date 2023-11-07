10d87f370843b50413638b6f5c2509875fbca6e8
Arturo1703 <arturolopez01.al@gmail.com>
2023-11-07 12:51:11 +0100

    props
    pasar datos de manera dinámica a un componente desde la app

--- ReactFirstApp/src/App.tsx
+++ ReactFirstApp/src/App.tsx
@@ -1,7 +1,12 @@
 import ListGroup from "./components/ListGroup";
 
-function App () {
-  return <div><ListGroup/></div>;
+function App() {
+  let items = ["Madrid", "Barcelona", "Valencia", "Cadiz", "Huesca"];
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
 }
 
 export default App;
--- ReactFirstApp/src/components/ListGroup.tsx
+++ ReactFirstApp/src/components/ListGroup.tsx
@@ -1,15 +1,36 @@
-function ListGroup() {
+import { MouseEvent, useState } from "react";
+
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+}
+function ListGroup({ items, heading }: ListGroupProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const getMessage = () => {
+    return items.length === 0 && <p>No item found</p>;
+  };
+
+  //const HandlerClick = () => {selectedIndex = index;};
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+      {getMessage}
       <ul className="list-group">
-        <li className="list-group-item active" aria-current="true">
-          An active item
-        </li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
