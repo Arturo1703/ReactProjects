96fa67acb9c4270f64e12594002df5fe8223e1ac
Arturo1703 <arturolopez01.al@gmail.com>
2023-11-11 22:22:15 +0100

    about 1/2

--- TutWebPersonal/src/components/about/about.jsx
+++ TutWebPersonal/src/components/about/about.jsx
@@ -1,10 +1,74 @@
-import React from 'react';
+import React from "react";
 import "./about.css";
+import Image from "../../assets/avatar-2.svg";
 
 const about = () => {
   return (
-    <div>about</div>
-  )
-}
+    <section className="about container section" id="about">
+      <h2 className="section__title">About Me</h2>
 
-export default about
+      <div className="about__container grid">
+        <img src={Image} alt="" className="about__img" />
+        <div className="about__data grid">
+          <div className="about__info">
+            <p className="about__description">
+              I am a computer engineer, passionate about computers, committed
+              and efficient in my work. I stand out in my problem solving
+              ability, communication skills and vast knowledge of digital
+              systems.
+            </p>
+            <a href="" className="btn">
+              Download CV
+            </a>
+          </div>
+
+          <div className="about__skills grid">
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Development</h3>
+                <span className="skills__number">70%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage development"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">UI/UX design</h3>
+                <span className="skills__number">50%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage design"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Communication</h3>
+                <span className="skills__number">90%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage communication"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">
+                  Will to learn and become better
+                </h3>
+                <span className="skills__number">100%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage involvment"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default about;
