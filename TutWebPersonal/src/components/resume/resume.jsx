f3a194126f34788f59da8f139a6e81cf5c227c80
Arturo1703 <arturolopez01.al@gmail.com>
2023-11-15 19:30:26 +0100

    portfolio

--- TutWebPersonal/src/components/resume/resume.jsx
+++ TutWebPersonal/src/components/resume/resume.jsx
@@ -1,30 +1,33 @@
 import React from "react";
+import Card from "./Card";
 import "./resume.css";
 import Data from "./Data";
-import Card from "./Card";
-const Resume = () => {
-  <section className="resume container section" id="resume">
-    <h1>Hello</h1>
-    <h2 className="section__title">Education</h2>
 
-    <div className="resume__container grid">
-      <div className="timeline grid">
-        {Data.map((val, id) => {
-          if (val.category == "education") {
-            return (
-              <Card
-                key={id}
-                icon={val.icon}
-                title={val.title}
-                year={val.year}
-                desc={val.desc}
-              />
-            );
-          }
-        })}
+const resume = () => {
+  return (
+    <section className="resume container section" id="resume">
+      <h1>Hello</h1>
+      <h2 className="section__title">Education</h2>
+
+      <div className="resume__container grid">
+        <div className="timeline grid">
+          {Data.map((val, id) => {
+            if (val.category == "education") {
+              return (
+                <Card
+                  key={id}
+                  icon={val.icon}
+                  title={val.title}
+                  year={val.year}
+                  desc={val.desc}
+                />
+              );
+            }
+          })}
+        </div>
       </div>
-    </div>
-  </section>;
+    </section>
+  );
 };
 
-export default Resume;
+export default resume;
