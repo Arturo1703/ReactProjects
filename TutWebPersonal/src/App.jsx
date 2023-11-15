1079b4dda35ae4be2e60c77fe38a2a36f6e183da
Arturo1703 <arturolopez01.al@gmail.com>
2023-11-15 16:41:37 +0100

    Resume pero no se displayea en la pagina y no aparece en el codigo del proyecto
    esta resume mal importado???

--- TutWebPersonal/src/App.jsx
+++ TutWebPersonal/src/App.jsx
@@ -1,36 +1,35 @@
-import { useState } from 'react';
-import './App.css';
-import Sidebar from './components/Sidebar/Sidebar'
-import Home from './components/home/home'
-import Portfolio from './components/portfolio/portfolio'
-import Services from './components/Services/Services'
-import Resume from './components/Resume/Resume'
-import About from './components/about/about'
-import Testimonials from './components/testimonials/testimonials'
-import Contact from './components/contact/contact';
-import Blog from './components/blog/blog'
-import Pricing from './components/pricing/pricing'
-
+import { useState } from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Home from "./components/home/home";
+import Portfolio from "./components/portfolio/portfolio";
+import Services from "./components/Services/Services";
+import Resume from "./components/resume/resume";
+import About from "./components/about/about";
+import Testimonials from "./components/testimonials/testimonials";
+import Contact from "./components/contact/contact";
+import Blog from "./components/blog/blog";
+import Pricing from "./components/pricing/pricing";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Sidebar/>
-      <main className='main'>
-        <Home/>
-        <About/>
-        <Services/>
-        <Resume/>
-        <Portfolio/>
-        <Pricing/>
-        <Contact/>
-        <Testimonials/>
-        <Blog/>
+      <Sidebar />
+      <main className="main">
+        <Home />
+        <About />
+        <Services />
+        <Resume />
+        <Portfolio />
+        <Pricing />
+        <Contact />
+        <Testimonials />
+        <Blog />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- TutWebPersonal/src/components/resume/resume.jsx
+++ TutWebPersonal/src/components/resume/resume.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import "./resume.jsx";
-const resume = () => {
-  return (
-    <div>resume</div>
-  )
-}
+import React from "react";
+import "./resume.css";
+import Data from "./Data";
+import Card from "./Card";
+const Resume = () => {
+  <section className="resume container section" id="resume">
+    <h1>Hello</h1>
+    <h2 className="section__title">Education</h2>
 
-export default resume
+    <div className="resume__container grid">
+      <div className="timeline grid">
+        {Data.map((val, id) => {
+          if (val.category == "education") {
+            return (
+              <Card
+                key={id}
+                icon={val.icon}
+                title={val.title}
+                year={val.year}
+                desc={val.desc}
+              />
+            );
+          }
+        })}
+      </div>
+    </div>
+  </section>;
+};
+
+export default Resume;
